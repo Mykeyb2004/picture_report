159a8e9b6e26ff8c24428ccd095faaabfaa1eecc
zhangqijin <keyb2004@qq.com>
2022-09-01 12:26:37 +0800

    首次提交

--- compress.py
+++ compress.py
@@ -6,7 +6,24 @@ import logfile
 
 
 class CompressPicture(object):
+    """
+        压缩图片文件
+    """
 
-    def __int__(self, base_width):
+    def __int__(self, base_width=1024):
+
         print("Compressing, base_width")
         self.base_width = base_width
+
+    def compress_ratio(self, w, h):
+        """
+        计算图片压缩比
+        :param w: 图片宽度
+        :param h: 图片高度
+        :return: 压缩比
+        """
+        if w < h:
+            w, h = h, w
+        # 计算压缩比
+        return 1 if w <= self.base_width else self.base_width / w
+
