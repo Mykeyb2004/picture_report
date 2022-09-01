c69fe91bb5836ff9655058ef833bfc3aeb943273
zhangqijin <keyb2004@qq.com>
2022-09-01 11:49:31 +0800

    首次提交

---
+++ compress.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+import logfile
+
+
+class CompressPicture(object):
+
+
+    def __int__(self, base_width):
+        print("Compressing, base_width")
+        self.base_width = base_width
